990fa158f775a70cc6957e81f7fa49b6456775ab
atsyplenkov <sharp.couch9400@fastmail.com>
2025-09-18 15:24:38 +1200

    package rename

--- R/bbox.R
+++ R/bbox.R
@@ -1,8 +1,18 @@
 #' Get the bounding box of a geometry
 #'
+#' @description
+#' Returns the bounding box of all geometries in the vector.
+#' For single geometries, the behavior of \code{geos_bbox} is
+#' identical to [geos_envelope][geos::geos_envelope].
+#'
 #' @param geom  A [GEOS geometry vector][geos::as_geos_geometry]
 #' @return A polygon [GEOS geometry vector][geos::as_geos_geometry]
 #' @export
+#'
+#' @examples
+#' x <- geos::as_geos_geometry("GEOMETRYCOLLECTION (POINT (1 2), POINT (3 4))")
+#' geos_bbox(x)
+#'
 geos_bbox <- function(geom) {
-  geos::geos_buffer(wk::wk_bbox(geom), 0)
+  geos::as_geos_geometry(wk::wk_bbox(geom))
 }
