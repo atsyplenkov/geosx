95db01eedd03ad96ae201166694c0c383763d911
atsyplenkov <sharp.couch9400@fastmail.com>
2025-09-25 16:12:31 +1200

    feat: geos_standalone_points

---
+++ R/points_within.R
@@ -0,0 +1,31 @@
+#' Find standalone points with no neighbors within a distance
+#'
+#' See: \url{https://lbusettspatialr.blogspot.com/2018/02/speeding-up-spatial-analyses-by.html}
+#'
+#' @param pts  [GEOS geometry vector][geos::as_geos_geometry] of points.
+#' @param maxdist Maximum distance to consider neighbors.
+#'
+#' @return [GEOS geometry vector][geos::as_geos_geometry]
+#'
+#' @export
+#'
+#' @examples
+#' maxdist <- 500
+#' pts <- data.frame(
+#'   x = runif(5000, 0, 100000),
+#'   y = runif(5000, 0, 100000)
+#' )
+#' pts_geom <- geos::as_geos_geometry(
+#'   wk::xy(pts$x, pts$y)
+#' )
+#' solo_pts <- geos_standalone_points(pts_geom, maxdist)
+#'
+geos_standalone_points <- function(pts, maxdist) {
+  pts_buffer <- geos::geos_buffer(pts, maxdist)
+  pts_str <- geos::geos_strtree(pts)
+  pts_ids <- geos::geos_intersects_matrix(pts_buffer, pts_str) |>
+    Filter(f = \(x) length(x) == 1) |>
+    unlist()
+
+  pts[pts_ids]
+}
