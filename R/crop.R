ea48b1e2400859f390b631e8b83904c1e932139f
atsyplenkov <sharp.couch9400@fastmail.com>
2026-03-19 16:48:18 +0300

    refactor: rename params

--- R/crop.R
+++ R/crop.R
@@ -1,30 +1,31 @@
 #' Clip geometries to a polygon
 #'
-#' This algorithm clips a vector layer using the features of a polygon layer.
+#' This algorithm clips a vector `input` layer using the features of a polygon
+#' `overlay` layer.
 #' Only the parts of the features in the input geom that fall within the
 #' polygons of the Overlay layer will be added to the resulting layer.
 #'
-#' @param geom  A [GEOS geometry vector][geos::as_geos_geometry]
-#' @param polygon A polygon [GEOS geometry vector][geos::as_geos_geometry]
+#' @param input  A [GEOS geometry vector][geos::as_geos_geometry]
+#' @param overlay A polygon [GEOS geometry vector][geos::as_geos_geometry]
 #'
 #' @return A [GEOS geometry vector][geos::as_geos_geometry]
 #' @export
-geos_crop <- function(geom, polygon) {
-  checkmate::assert_multi_class(geom, c("geos_geometry", "wk_xy"))
-  checkmate::assert_class(polygon, "geos_geometry")
+geos_crop <- function(input, overlay) {
+  checkmate::assert_multi_class(input, c("geos_geometry", "wk_xy"))
+  checkmate::assert_class(overlay, "geos_geometry")
   checkmate::assert_true(
-    all(geos::geos_type(polygon) %in% c("polygon", "multipolygon"))
+    all(geos::geos_type(overlay) %in% c("polygon", "multipolygon"))
   )
 
   geos::geos_intersection(
-    geos_str_intersection(geom, polygon),
-    polygon
+    geos_str_intersection(input, overlay),
+    overlay
   )
 }
 
 #' @rdname geos_crop
 #' @aliases geos_clip
 #' @export
-geos_clip <- function(geom, polygon) {
-  geos_crop(geom, polygon)
+geos_clip <- function(input, overlay) {
+  geos_crop(input, overlay)
 }
